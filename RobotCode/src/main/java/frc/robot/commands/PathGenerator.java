0e34d8b1edad97bcf158d838d3b4c72fcc2cf6c1
DashGuy999 <64654064+DashGuy999@users.noreply.github.com>
2022-09-24 20:03:11 -0400

    Working non-parametric solver

--- RobotCode/src/main/java/frc/robot/commands/PathGenerator.java
+++ RobotCode/src/main/java/frc/robot/commands/PathGenerator.java
@@ -18,127 +18,226 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class PathGenerator extends CommandBase {
   
-  private final double[] endChunk = {-1,-1};
-  private final double[] endPath = {-2,-2};
+  double[][] coords = {{1,1}, {2,3}, {3,-2}, {4,5}};
+  double beginningSlope = 0;
+  double endingSlope = -1;
 
+  int subscript = 0;
+  int pow = 0;
+  int coef = 1;
+  int matrixOffset = 0;
 
-  private double[][] allCoordinates = 
-  {{0,0}, {1,1}, endChunk,
-  {1,1}, {2,1}, {6,5}, {10,6}, endChunk,
-  endPath};
+  //A really big matrix. Contains all of the function constraints and modified x values
+  SimpleMatrix megaMatrix = new SimpleMatrix((coords.length-1)*4, (coords.length-1)*4);
+  
+  //Holds the y values and specified slopes. It is used with megaMatrix to solve for the coeffecients
+  SimpleMatrix yMatrix = new SimpleMatrix((coords.length-1)*4, 1);
+  
+  //Holds all of the calculated coeffecients of each function
+  SimpleMatrix coefMatrix = new SimpleMatrix(1, (coords.length-1)*4);
 
-  private double[][] chunk = new double[4][3];
-  private double[] coordinate = new double[3];
-  private int chunkLength = 0;
-  private int uImprint = 0;
-
-  private SimpleMatrix A;
-  private SimpleMatrix B;
-  private SimpleMatrix C;
-    
-  private String queryString = "";
-
-
+  
+  //Used to order the matrix rows on the sim SmartDashboard (they come up in alphabetical order)
+  String[] theAlphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"};
+  
   public PathGenerator() {
 
-      
 
-    for(int outerLoop = 0; coordinate != endPath; outerLoop++){
+    //// FILL MEGAMATRIX START (x matrix) ////////////////////////////////////////////////////////////////////////////////////
+    
+   
+    //Make sure that the starting point of the path has the correct slope (fill line 1)
+    matrixOffset = 1;
+    for(int i = 0; i < 3; i++){
+      megaMatrix.set(0, i + matrixOffset, coef * Math.pow(coords[subscript][0], pow));
+      pow++;
+      coef++;
+    }
+    pow = 0;
+    coef = 0;
+    matrixOffset = 0;
+    
 
-            
-            
-      coordinate = allCoordinates[outerLoop];
 
-      if(coordinate != endChunk){
-          SmartDashboard.putString("Trip2", "Filling Chunks");
-          chunk[chunkLength][0] = coordinate[0];
-          chunk[chunkLength][1] = coordinate[1];
-          chunk[chunkLength][2] = uImprint;
-          uImprint++;
-          chunkLength++;
-          continue;
+    //Fill middle functions
+    //Makes sure that all functions not touching the overall start/endpoints of the path
+    //have matching 1st/2nd derivatives at the points they touch,
+    //as well as making sure that they pass through those said points
+
+    for(int bigLoop = 0; bigLoop < coords.length-2; bigLoop++){
+
+      //Used to determine which row to put numbers on
+      int n = bigLoop * 4;
+
+      //Make sure that the current function touches its first endpoint
+      for(int i = 0; i < 4; i++){
+        megaMatrix.set(n+1, i + matrixOffset, Math.pow(coords[subscript][0], pow));
+        pow++;
       }
+      pow = 0;
 
-      A = new SimpleMatrix(chunkLength, chunkLength);
-      B = new SimpleMatrix(chunkLength, 1);
-      C = new SimpleMatrix(chunkLength,2);
-
-      for(int i = 0; i < chunkLength; i++){
-          SmartDashboard.putString("Trip3", "Inner Loop 1");
-          for(int j = 0; j < chunkLength; j++){
-              A.set(i, j, Math.pow(chunk[i][2], j));
-          }
-          
+      //Make sure that the current function touches its second endpoint
+      subscript++;
+      for(int i = 0; i < 4; i++){
+        megaMatrix.set(n+2, i + matrixOffset, Math.pow(coords[subscript][0], pow));
+        pow++;
       }
-  
-      
+      pow = -1;
+
+      //Make sure that both funcitons touching the 2nd endpoint have equal 1st derivatives
+      //There are 2 "parts" to this line, with the 2nd being the inverse of the 1st
+      int invert = 1;
+      int inlineOffset = 0;
       for(int i = 0; i < 2; i++){
-          SmartDashboard.putString("Trip4", "Inner Loop 2");
-          for(int j = 0; j < chunkLength; j++){
-              B.set(j, 0, chunk[j][i]);
-          }
-          SimpleMatrix invA = A.invert();
+        for(int j = 0; j < 4; j++){
+          megaMatrix.set(n+3, j + inlineOffset + matrixOffset, coef * Math.pow(coords[subscript][0], pow) * invert);
+          pow++;
+          coef++;
+        }
+        pow = -1;
+        coef = 0;
+        invert = -1;
+        inlineOffset = 4;
+      }
+      invert = 1;
+      inlineOffset = 0;
+      pow = 0;
+      coef = 0;
 
-          C.insertIntoThis(0, i, invA.mult(B)); 
+      //Make sure that both funcitons touching the 2nd endpoint have equal 2nd derivatives
+      //Again, there are 2 "parts" to this line, with the 2nd being the inverse of the 1st
+      for(int i = 0; i < 2; i++){
+        megaMatrix.set(n+4, inlineOffset + matrixOffset, 0);
+        megaMatrix.set(n+4, inlineOffset + 1 + matrixOffset, 0);
+        megaMatrix.set(n+4, inlineOffset + 2 + matrixOffset, 2 * invert);
+        megaMatrix.set(n+4, inlineOffset + 3 + matrixOffset, 6 * coords[subscript][0] * invert);
+
+        inlineOffset = 4;
+        invert = -1;
       }
 
-      
+      //Then repeat for all middle functions, but shift all of these matrix fillers right 4 rows
+      matrixOffset = matrixOffset + 4;
 
-      //Setting up query string
-      //Testing query string: ?u=1&x=1&x=2&y=1&y=2&br&u=4&x=2&x=3&y=4&y=2&
-      
-      queryString = queryString + "u=" + chunk[chunkLength-1][2] + "&";
-
-      for(int i = 0; i < C.numRows(); i++){
-          BigDecimal rounder = new BigDecimal(C.get(i, 0));
-          BigDecimal rounded = rounder.setScale(4, RoundingMode.FLOOR);
-          queryString = queryString + "x=" + rounded + "&";
-      }
-      for(int i = 0; i < C.numRows(); i++){
-          BigDecimal rounder = new BigDecimal(C.get(i, 1));
-          BigDecimal rounded = rounder.setScale(4, RoundingMode.FLOOR);
-          queryString = queryString + "y=" + rounded + "&";
-      }
-      queryString = queryString + "br&";
-      
-      uImprint--;
-
-      chunkLength = 0;
     }
-    SmartDashboard.putString("Trip", "After Loop");
-  
-    //Shave off extra br&
-    queryString = queryString.substring(0,queryString.length()-3);
-  
-    //Opening Desmos
-  
-    try {
 
-      URI desmosURI = new URI("https://atlee-circuitree.github.io/CircuitWeaveV2/");
+    
+    
+    //We still have 3 lines to go, each correspoding to the last function
 
-      //SmartDashboard.putString("desmosURI.getQuery before", desmosURI.getQuery());
-
-      
-      
-      URI newDesmosURI = new URI(desmosURI.getScheme(), desmosURI.getAuthority(),
-      desmosURI.getPath(), queryString, desmosURI.getFragment());
-
-      SmartDashboard.putString("Generated URL", newDesmosURI.toString());
-      SmartDashboard.putString("desmosURI.getQuery after", newDesmosURI.getQuery());
-      
-      Desktop.getDesktop().browse(newDesmosURI);
-      
-
-
-    } 
-    catch (URISyntaxException e) {
-      SmartDashboard.putString("ERROR", "Something Went Wrong (catch1)");
+    //Make sure that the last function touches its first endpoint
+    for(int i = 0; i < 4; i++){
+      megaMatrix.set(megaMatrix.numRows()-3, i + matrixOffset, Math.pow(coords[subscript][0], pow));
+      pow++;
     }
-    catch(IOException e){
-      SmartDashboard.putString("ERROR", "Something Went Wrong (catch2)");
+    pow = 0;
+
+    //Make sure that the last function touches the endpoint for the whole path
+    subscript++;
+    for(int i = 0; i < 4; i++){
+      megaMatrix.set(megaMatrix.numRows()-2, i + matrixOffset, Math.pow(coords[subscript][0], pow));
+      pow++;
+    }
+    pow = 0;
+    coef = 1;
+
+    //Make sure that the ending point of the path has the correct slope (fill last line)
+    matrixOffset = matrixOffset + 1;
+    for(int i = 0; i < 3; i++){
+      megaMatrix.set(megaMatrix.numRows()-1, i + matrixOffset, coef * Math.pow(coords[subscript][0], pow));
+      pow++;
+      coef++;
     }
 
 
+    //// FILL MEGAMATRIX END /////////////////////////////////////////////////////////////////////////////////////////
+
+
+    
+    //Now we fill the Y Matrix, which will be used to solve for the coeffecients
+
+    //Put the 1st specified slope as the top spot
+    yMatrix.set(0, 0, beginningSlope);
+    
+    //Manually fill the 2nd line to make the for loop more effecient
+    yMatrix.set(1, 0, coords[0][1]);
+
+    subscript = 1;
+
+    //fill the middle function y values
+    for(int i = 0; i < coords.length-2; i++){
+      
+      //used to determine which row to put values on
+      int n = i * 4;
+
+      yMatrix.set(n + 2, 0, coords[subscript][1]);
+      yMatrix.set(n + 3, 0, 0);
+      yMatrix.set(n + 4, 0, 0);
+      yMatrix.set(n + 5, 0, coords[subscript][1]);
+
+      subscript++;
+
+    }
+
+    //Manually fill the 2nd to last line
+    yMatrix.set(yMatrix.numRows()-2, 0, coords[subscript][1]);
+
+    //Set the 2nd specified slope as the last line
+    yMatrix.set(yMatrix.numRows()-1, 0, endingSlope);
+
+
+
+    coefMatrix = megaMatrix.invert().mult(yMatrix);
+
+
+
+
+    //Put megaMatrix onto SmartDashboard
+    String[] megaString = matrixToStringArray(megaMatrix);
+
+    for(int i = 1; i < megaString.length+1; i++){
+      SmartDashboard.putString(theAlphabet[i-1] + " Row " + i , megaString[i-1]);
+    }
+
+
+    //Put yMatrix onto SmartDashboard
+    String yString = "";
+
+    for(int i = 0; i < yMatrix.numRows(); i++){
+      yString = yString + String.valueOf(yMatrix.get(i, 0)) + ", ";
+    }
+    SmartDashboard.putString("Y Matrix", yString);
+
+
+    //Put coefMatrix onto SmartDashboard
+    String coefString = "";
+
+    for(int i = 0; i < coefMatrix.numRows(); i++){
+      coefString = coefString + String.valueOf(coefMatrix.get(i, 0)) + ", ";
+    }
+    SmartDashboard.putString("Z Coef Matrix", coefString);
+
+
+
+
+  }
+
+  
+
+
+  //Converts a SimpleMatrix to a String[]
+  public String[] matrixToStringArray(SimpleMatrix matrix){
+
+    String[] stringArray = new String[matrix.numRows()];
+    
+    for(int i = 0; i < matrix.numRows(); i++){
+      stringArray[i] = " ";
+      for(int j = 0; j < matrix.numCols(); j++){
+        stringArray[i] = stringArray[i] + String.valueOf(matrix.get(i, j)) + ", ";
+      }
+    }
+    
+    return stringArray;
   }
 
   // Called when the command is initially scheduled.
